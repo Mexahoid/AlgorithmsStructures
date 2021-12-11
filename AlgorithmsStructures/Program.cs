6b3d6eaee0064d7712345b7f3842456ba3e198cb
Mexahoid <mirramax97@Mail.ru>
2021-12-11 16:18:31 +0300

    Added Lists
    Related to #1.

--- AlgorithmsStructures/Program.cs
+++ AlgorithmsStructures/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Structures.List;
+
+MyArrayList<int> arrayList = new();
+MyLinkedList<int> linkedList = new();
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
---
+++ Structures/List/MyArrayList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures.List
+{
+    public class MyArrayList<T>
+    {
+        public MyArrayList()
+        {
+
+        }
+    }
+}
