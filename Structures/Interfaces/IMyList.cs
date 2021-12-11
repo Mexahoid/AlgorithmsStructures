da7674cabbd150664842f9e59b92f179f44bed37
Mexahoid <mirramax97@Mail.ru>
2021-12-11 19:00:57 +0300

    Extracted count method to an interface.

--- Structures/Interfaces/IMyList.cs
+++ Structures/Interfaces/IMyList.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Structures.Interfaces
 {
-    public interface IMyList<T>
+    public interface IMyList<T> : ICountable
     {
-        public int Count { get; }
-
         public void Add(T input);
 
         public void Clear();
