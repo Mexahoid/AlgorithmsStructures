9ba512e3e14b233cc0f1036975de8e77bbb250db
Mexahoid <mirramax97@Mail.ru>
2021-12-11 19:21:47 +0300

    Added queue interface.
    Related to #2.

---
+++ Structures/Interfaces/IMyQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures.Interfaces
+{
+    internal interface IMyQueue<T>
+    {
+        public bool IsEmpty { get; }
+
+        public void Enqueue(T item);
+
+        public void Enqueue(IEnumerable<T> items);
+
+        public T Dequeue();
+    }
+}
