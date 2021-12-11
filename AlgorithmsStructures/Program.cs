787cc2ce71b092cac24272d3ade03767c6f1814f
Mexahoid <mirramax97@Mail.ru>
2021-12-11 17:41:14 +0300

    Added ArrayList
    Related to #1.

--- AlgorithmsStructures/Program.cs
+++ AlgorithmsStructures/Program.cs
@@ -1,44 +1,94 @@
 ﻿using Structures.Interfaces;
 using Structures.List;
 
+IMyList<int> arrayList = new MyArrayList<int>(8);
 IMyList<int> linkedList = new MyLinkedList<int>();
 
-
-for (int i = 0; i < 10; i++)
-{
-    linkedList.Add(i);  
-}
-
-for (int i = 0; i < linkedList.Count; i++)
-{
-    Console.WriteLine($">> {linkedList[i]}");
-}
-
-for (int i = 0; i < linkedList.Count; i++)
-{
-    linkedList[i] = linkedList[i] * 10;
-}
-
-for (int i = 0; i < linkedList.Count; i++)
-{
-    Console.WriteLine($">> {linkedList[i]}");
-}
-
-for (int i = linkedList.Count - 1; i >= 0; i--)
-{
-    linkedList.RemoveAt(i);
-}
-
-for (int i = 0; i < 10; i++)
+int count = 10;
+for (int i = 0; i < count; i++)
 {
     linkedList.Add(i);
+    arrayList.Add(i);
+}
+Console.WriteLine("Linked");
+for (int i = 0; i < linkedList.Count; i++)
+{
+    Console.WriteLine($">> {linkedList[i]}");
+}
+Console.WriteLine("Array");
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine($">> {arrayList[i]}");
 }
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < count; i++)
+{
+    linkedList[i] = linkedList[i] * 10;
+    arrayList[i] = arrayList[i] * 5;
+}
+
+Console.WriteLine("Linked");
+for (int i = 0; i < linkedList.Count; i++)
+{
+    Console.WriteLine($">> {linkedList[i]}");
+}
+Console.WriteLine("Array");
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine($">> {arrayList[i]}");
+}
+
+for (int i = count - 1; i >= 0; i--)
+{
+    linkedList.RemoveAt(i);
+    arrayList.RemoveAt(i);
+}
+
+Console.WriteLine($"Count: {linkedList.Count}, {arrayList.Count}");
+Console.WriteLine("Linked");
+for (int i = 0; i < linkedList.Count; i++)
+{
+    Console.WriteLine($">> {linkedList[i]}");
+}
+Console.WriteLine("Array");
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine($">> {arrayList[i]}");
+}
+
+for (int i = 0; i < count; i++)
+{
+    linkedList.Add(i);
+    arrayList.Add(i);
+}
+
+Console.WriteLine("Linked");
+for (int i = 0; i < linkedList.Count; i++)
+{
+    Console.WriteLine($">> {linkedList[i]}");
+}
+Console.WriteLine("Array");
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine($">> {arrayList[i]}");
+}
+
+for (int i = 0; i < count; i++)
 {
     linkedList.Remove(i);
+    arrayList.Remove(i);
 }
-Console.WriteLine($"Count: {linkedList.Count}");
+Console.WriteLine($"Count: {linkedList.Count}, {arrayList.Count}");
+Console.WriteLine("Linked");
+for (int i = 0; i < linkedList.Count; i++)
+{
+    Console.WriteLine($">> {linkedList[i]}");
+}
+Console.WriteLine("Array");
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine($">> {arrayList[i]}");
+}
 
 Console.WriteLine($"Hello, World!");
 
--- Structures/List/MyArrayList.cs
+++ Structures/List/MyArrayList.cs
@@ -1,16 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Structures.Interfaces;
 
 namespace Structures.List
 {
-    public class MyArrayList<T>
+    public class MyArrayList<T> : IMyList<T>
     {
-        public MyArrayList()
-        {
+        private readonly int _size;
+        private int _ptr;
+        private T[] _array;
 
+        public MyArrayList(int size)
+        {
+            if (size < 1)
+                throw new ArgumentOutOfRangeException(nameof(size));
+            _size = size;
+            _ptr = 0;
+            _array = new T[size];
+        }
+
+        public T this[int index] { get => _array[index]; set => _array[index] = value; }
+
+        public int Count => _ptr;
+
+        public void Add(T input)
+        {
+            _array[_ptr++] = input;
+
+            if (_ptr >= _array.Length)
+            {
+                ResizeArray(true);
+            }
+        }
+
+        public void Clear()
+        {
+            _array = new T[_size];
+            _ptr = 0;
+        }
+
+        private void ResizeArray(bool increase)
+        {
+            T[] newArray;
+            int count;
+            int newSize;
+            if (increase)
+            {
+                newSize = _array.Length + _size;
+                count = _array.Length;
+            }
+            else
+            {
+                newSize = _array.Length - _size;
+                count = newSize;
+            }
+            newArray = new T[newSize];
+            for (int i = 0; i < count; i++)
+                newArray[i] = _array[i];
+            _array = newArray;
+        }
+
+        public void Remove(T value)
+        {
+            int i;
+            for (i = 0; i < _array.Length; i++)
+            {
+                if (_array[i].Equals(value))
+                    break;
+            }
+            RemoveAt(i);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < _array.Length - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            _ptr--;
+
+            if (_ptr < _array.Length - _size)
+            {
+                ResizeArray(false);
+            }
         }
     }
 }
