911bd79d65b237ae5990eb6d65e8a59ee33378a9
Mexahoid <mirramax97@Mail.ru>
2021-12-30 17:00:18 +0300

    Added lazy singleton.
    Related to #15.

---
+++ Patterns/Singleton/LazySingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Singleton
+{
+    internal class LazySingleton
+    {
+        private class Nested
+        {
+            static Nested()
+            {
+
+            }
+
+            public static readonly LazySingleton Instance = new();
+        }
+
+        public static LazySingleton Instance
+        {
+            get
+            {
+                return Nested.Instance;
+            }
+        }
+
+        public int Value { get; set; }
+
+        private LazySingleton()
+        {
+
+        }
+    }
+}
