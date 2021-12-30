ae8d26776a26202740027739080e7b665bf7caab
Mexahoid <mirramax97@Mail.ru>
2021-12-30 17:39:29 +0300

    Added interface.
    Related to #16.

---
+++ Patterns/Prototype/IPrototype.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Prototype
+{
+    internal interface IPrototype
+    {
+        public IPrototype Clone();
+    }
+}
