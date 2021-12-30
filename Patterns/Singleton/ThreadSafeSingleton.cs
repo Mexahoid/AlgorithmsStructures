468a35ef2bfff1a65b3f8c93fbde31f50e2139eb
Mexahoid <mirramax97@Mail.ru>
2021-12-30 17:02:37 +0300

    Added thread-safe singleton.
    Related to #15.

---
+++ Patterns/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Singleton
+{
+    internal class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton _instance;
+
+        public int Value { get; set; }
+
+        private static readonly object _lock = new();
+
+        private ThreadSafeSingleton()
+        {
+
+        }
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                            _instance = new ThreadSafeSingleton();
+                    }
+                return _instance;
+            }
+        }
+    }
+}
