8b57df6b5c36ae62a522fa80093194d5543cb823
Mexahoid <mirramax97@Mail.ru>
2021-12-30 17:44:52 +0300

    Added first real prototype.
    Related to #16.

---
+++ Patterns/Prototype/RealPrototype.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Prototype
+{
+    internal abstract class RealPrototype : IPrototype
+    {
+        private (int x, int y) _point;
+
+        public RealPrototype(int x, int y)
+        {
+            _point = (x, y);
+        }
+
+        public RealPrototype(RealPrototype source)
+        {
+            _point.x = source._point.x;
+            _point.y = source._point.y;
+        }
+
+        public virtual void Print(Action<string> printer)
+        {
+            printer?.Invoke($"Center is on x: {_point.x}, y: {_point.y}");
+        }
+
+        public abstract IPrototype Clone();
+    }
+}
