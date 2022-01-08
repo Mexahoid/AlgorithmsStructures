784e11445dd99a639eee33fb71f473f363db4b62
Mexahoid <mirramax97@Mail.ru>
2022-01-08 15:18:54 +0300

    Added interfaces.
    Related to #18.

---
+++ Patterns/AbstractFactory/IAbstractFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.AbstractFactory
+{
+    internal interface IAbstractFactory
+    {
+        public IProductA CreateProductA();
+
+        public IProductB CreateProductB();
+    }
+}
---
+++ Patterns/AbstractFactory/IProductA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.AbstractFactory
+{
+    internal interface IProductA
+    {
+        public string Name { get; set; }
+    }
+}
