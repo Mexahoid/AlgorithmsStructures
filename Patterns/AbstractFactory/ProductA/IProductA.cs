f316c1406f315ef8fed4ad41eda92cbc503339ac
Mexahoid <mirramax97@Mail.ru>
2022-01-08 15:21:57 +0300

    Moved interfaces.
    Related to #18.

--- Patterns/AbstractFactory/ProductA/IProductA.cs
+++ Patterns/AbstractFactory/ProductA/IProductA.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Patterns.AbstractFactory
+namespace Patterns.AbstractFactory.ProductA
 {
     internal interface IProductA
     {
