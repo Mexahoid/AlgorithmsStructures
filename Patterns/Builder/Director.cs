ced2e5e17772242ac7e6fc73eeefc3a1186ef882
Mexahoid <mirramax97@Mail.ru>
2022-01-08 15:36:56 +0300

    Added main builder classes.
    Related to #19.

---
+++ Patterns/Builder/Director.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Builder
+{
+    internal class Director
+    {
+        private IBuilder _builder;
+
+        public Director(IBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void SetBuilder(IBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void Make()
+        {
+            _builder.Reset();
+
+            _builder.BuildStepA();
+            _builder.BuildStepB();
+            _builder.BuildStepC();
+        }
+    }
+}
---
+++ Patterns/Builder/IBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Builder
+{
+    internal interface IBuilder
+    {
+        public void Reset();
+
+        public void BuildStepA();
+        public void BuildStepB();
+        public void BuildStepC();
+    }
+}
