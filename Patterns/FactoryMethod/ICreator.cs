34e2a6700338bd5712306ec02f4828f950a07a71
Mexahoid <mirramax97@Mail.ru>
2022-01-08 15:04:36 +0300

    Added interfaces.
    Related to #17.

---
+++ Patterns/FactoryMethod/ICreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.FactoryMethod
+{
+    internal interface ICreator
+    {
+        public void Operation();
+
+        IProduct CreateProduct();
+    }
+}
