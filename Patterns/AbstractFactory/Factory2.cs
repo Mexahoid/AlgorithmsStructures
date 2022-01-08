6b9c34ce4e81aa98f0a77f1b0fc0d36508f2622d
Mexahoid <mirramax97@Mail.ru>
2022-01-08 15:26:52 +0300

    Added concrete factories.
    Related to #18.

---
+++ Patterns/AbstractFactory/Factory2.cs
@@ -0,0 +1,23 @@
+﻿using Patterns.AbstractFactory.ProductA;
+using Patterns.AbstractFactory.ProductB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.AbstractFactory
+{
+    internal class Factory2 : IAbstractFactory
+    {
+        public IProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public IProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+}
